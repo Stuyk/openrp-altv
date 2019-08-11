5ce3144a6ca5e751a9a9766fffa8f4353df28d7c
Stuyk <stuykgaming@gmail.com>
2019-08-11 17:31:28 -0600

    Absolute Chaos

--- client/customizers/character.mjs
+++ client/customizers/character.mjs
@@ -1,9 +1,6 @@
 import * as alt from 'alt';
 import * as native from 'natives';
 
-// mp_m_freemode_01
-// mp_f_freemode_01
-
 const cameraPoint = {
     x: -140.7032928466797,
     y: -644.9724975585938,
@@ -16,15 +13,15 @@ const playerPoint = {
     z: 168.813232421875
 };
 
-var webView = undefined;
-var characterCamera = undefined;
-var modPed = undefined;
-var fov = 28;
+var webView = undefined; // Used for the HTML View.
+var characterCamera = undefined; // Used for the Camera Manipulation
+var modPed = undefined; // The pedestrian we create.
+var fov = 28; // The FOV we change with scroll wheel.
 var [_dontCare, screenWidth, screenHeight] = native.getActiveScreenResolution(
     0,
     0
-);
-var lastHair = 0;
+); // Get the current screen resolution the user is using.
+var lastHair = 0; // Get the last hair the player set.
 
 // Load the character customizer, freeze controls, create camera, and ped.
 export function loadCharacterCustomizer() {
@@ -36,6 +33,7 @@ export function loadCharacterCustomizer() {
         0
     );
 
+    // Setup a temporary teleport, and turn off the radar.
     alt.emitServer('temporaryTeleport', cameraPoint);
     native.displayRadar(false);
 
@@ -53,10 +51,11 @@ export function loadCharacterCustomizer() {
         0
     );
 
+    // Request these models if they're not already loaded.
     native.requestModel(native.getHashKey('mp_m_freemode_01'));
     native.requestModel(native.getHashKey('mp_f_freemode_01'));
 
-    // Create a ped.
+    // Create a pedestrian to customize.
     modPed = native.createPed(
         26,
         native.getHashKey('mp_f_freemode_01'),
@@ -68,6 +67,7 @@ export function loadCharacterCustomizer() {
         false
     );
 
+    // Set the head blend data to 0 to prevent texture issues.
     native.setPedHeadBlendData(modPed, 0, 0, 0, 0, 0, 0, 0, 0, 0, false);
 
     // Point camera at entity; with no offset.
@@ -88,6 +88,7 @@ export function loadCharacterCustomizer() {
     alt.showCursor(true);
 
     // Setup the events for the webview.
+    // Update the player's face.
     webView.on(
         'updateHeadBlend',
         // fFace - Father Face, mFace - Mother Face, tFace - Third Skin
@@ -120,6 +121,7 @@ export function loadCharacterCustomizer() {
         }
     });
 
+    // Update Hair Style
     webView.on('updateHairStyle', (hair, colorA, colorB, hairTexture) => {
         native.setPedComponentVariation(modPed, 2, hair, hairTexture, 0);
         native.setPedHairColor(modPed, colorA, colorB);
@@ -136,12 +138,174 @@ export function loadCharacterCustomizer() {
             webView.emit('setHairTextureVariations', hairTextureVariations);
         }
     });
+
+    // Update Facial Features - Nose, and stuff. f = face feature
+    // Names are listed inside of the app.js for the names of the ff features.
+    webView.on('updateFacialFeatures', (eyeColor, faceValuesJSON) => {
+        native.setPedEyeColor(modPed, eyeColor);
+        let faceValues = JSON.parse(faceValuesJSON);
+
+        for (var index in faceValues) {
+            native.setPedFaceFeature(modPed, index, faceValues[index]);
+        }
+    });
+
+    webView.on('updateFacialDecor', arrayOfDecor => {
+        let decorData = JSON.parse(arrayOfDecor);
+
+        alt.log(arrayOfDecor);
+
+        for (var index = 0; index < decorData.length; index++) {
+            // Blemish, Blemish Opacity
+            if (index === 0) {
+                native.setPedHeadOverlay(
+                    modPed,
+                    0,
+                    decorData[index],
+                    decorData[index + 1]
+                );
+
+                alt.log('yep that a blemish');
+            }
+
+            // Facial Hair
+            if (index === 2) {
+                native.setPedHeadOverlay(
+                    modPed,
+                    1,
+                    decorData[index],
+                    decorData[index + 1]
+                );
+
+                native.setPedHeadOverlayColor(
+                    modPed,
+                    1,
+                    1,
+                    decorData[index + 2],
+                    decorData[index + 3]
+                );
+            }
+
+            // Eyebrows, Eyebrow Opacity, Eyebrow Color 1, Color 2
+            if (index === 6) {
+                native.setPedHeadOverlay(
+                    modPed,
+                    2,
+                    decorData[index],
+                    decorData[index + 1]
+                );
+
+                native.setPedHeadOverlayColor(
+                    modPed,
+                    2,
+                    1,
+                    decorData[index + 2],
+                    decorData[index + 3]
+                );
+            }
+
+            // Ageing
+            if (index === 10) {
+                native.setPedHeadOverlay(
+                    modPed,
+                    3,
+                    decorData[index],
+                    decorData[index + 1]
+                );
+            }
+
+            // Makeup
+            if (index === 12) {
+                native.setPedHeadOverlay(
+                    modPed,
+                    4,
+                    decorData[index],
+                    decorData[index + 1]
+                );
+
+                native.setPedHeadOverlayColor(
+                    modPed,
+                    4,
+                    2,
+                    decorData[index + 2],
+                    decorData[index + 3]
+                );
+            }
+
+            // Blush
+            if (index === 16) {
+                native.setPedHeadOverlay(
+                    modPed,
+                    5,
+                    decorData[index],
+                    decorData[index + 1]
+                );
+
+                native.setPedHeadOverlayColor(
+                    modPed,
+                    5,
+                    2,
+                    decorData[index + 2],
+                    decorData[index + 2]
+                );
+            }
+
+            // Complexion
+            if (index === 19) {
+                native.setPedHeadOverlay(
+                    modPed,
+                    6,
+                    decorData[index],
+                    decorData[index + 1]
+                );
+            }
+
+            // Sun Damage
+            if (index === 21) {
+                native.setPedHeadOverlay(
+                    modPed,
+                    7,
+                    decorData[index],
+                    decorData[index + 1]
+                );
+            }
+
+            // Lipstick
+            if (index === 23) {
+                native.setPedHeadOverlay(
+                    modPed,
+                    8,
+                    decorData[index],
+                    decorData[index + 1]
+                );
+
+                native.setPedHeadOverlayColor(
+                    modPed,
+                    8,
+                    2,
+                    decorData[index + 2],
+                    decorData[index + 3]
+                );
+            }
+
+            // Freckles
+            if (index === 27) {
+                native.setPedHeadOverlay(
+                    modPed,
+                    9,
+                    decorData[index],
+                    decorData[index + 1]
+                );
+            }
+        }
+    });
 }
 
 function resetCamera(modelToUse) {
+    // Delete the new ped.
     native.deletePed(modPed);
 
-    // Create a ped.
+    // Re-Create the Ped
     modPed = native.createPed(
         26,
         modelToUse,
@@ -153,12 +317,15 @@ function resetCamera(modelToUse) {
         false
     );
 
+    // Set the head blend data to 0 to prevent weird hair texture glitches. Thanks Matspyder
     native.setPedHeadBlendData(modPed, 0, 0, 0, 0, 0, 0, 0, 0, 0, false);
 
+    // Fetch number of styles from natives.
     var beardStyles = native.getNumberOfPedDrawableVariations(modPed, 1);
     var hairStyles = native.getNumberOfPedDrawableVariations(modPed, 2);
     var hairColors = native.getNumHairColors();
 
+    // Emit the webview to update the style choices.
     webView.emit('stylesUpdate', beardStyles, hairStyles, hairColors);
 
     // Point camera at entity; with no offset.
@@ -168,14 +335,19 @@ function resetCamera(modelToUse) {
     native.renderScriptCams(true, false, 0, true, false);
 }
 
+// Called Constantly
 alt.on('update', () => {
     if (characterCamera === undefined) return;
 
+    // Disable Controls for the Player
     native.disableAllControlActions(0);
     native.disableAllControlActions(1);
+    let cursorRelativePos = alt.getCursorPos().x;
 
     // Scroll to zoom in.
     if (native.isDisabledControlJustPressed(0, 14)) {
+        if (cursorRelativePos < screenWidth / 2) return;
+
         fov += 1;
         if (fov >= 29) fov = 28;
         updateCamera();
@@ -183,6 +355,8 @@ alt.on('update', () => {
 
     // Scroll to zoom out
     if (native.isDisabledControlJustPressed(0, 15)) {
+        if (cursorRelativePos < screenWidth - screenWidth / 2) return;
+
         fov -= 1;
         if (fov <= 16) fov = 17;
         updateCamera();
@@ -192,14 +366,11 @@ alt.on('update', () => {
     if (native.isDisabledControlPressed(0, 25)) {
         let heading = native.getEntityHeading(modPed);
 
-        // So it'll go negative.
-        let cursorRelativePos = alt.getCursorPos().x;
-
         if (cursorRelativePos < screenWidth / 2) {
             cursorRelativePos = -0.5;
         }
 
-        if (cursorRelativePos > screenWidth / 2) {
+        if (cursorRelativePos > screenWidth - screenWidth / 2) {
             cursorRelativePos = 0.5;
         }
 
@@ -207,11 +378,13 @@ alt.on('update', () => {
     }
 });
 
+// Update the Camera when the FOV changes.
 function updateCamera() {
     native.setCamFov(characterCamera, fov);
     native.renderScriptCams(true, false, 0, true, false);
 }
 
+// Delete the additional ped created on disconnect.
 alt.on('disconnect', () => {
     native.deletePed(modPed);
 });
--- client/html/character/app.js
+++ client/html/character/app.js
@@ -1,92 +1,215 @@
-const facialFeatures = [
-    { name: 'Sex', value: 0, min: 0, max: 1, increment: 1 },
-    { name: 'Father Face', value: 0, min: 0, max: 45, increment: 1 },
-    { name: 'Father Skin', value: 0, min: 0, max: 45, increment: 1 },
-    { name: 'Mother Face', value: 0, min: 0, max: 45, increment: 1 },
-    { name: 'Mother Skin', value: 0, min: 0, max: 45, increment: 1 },
-    { name: 'Extra Face', value: 0, min: 0, max: 45, increment: 1 },
-    { name: 'Extra Skin', value: 0, min: 0, max: 45, increment: 1 },
-    { name: 'Face Mix', value: 0, min: 0, max: 1, increment: 0.1 },
-    { name: 'Skin Mix', value: 0, min: 0, max: 1, increment: 0.1 },
-    { name: 'Third Mix', value: 0, min: 0, max: 1, increment: 0.1 },
-    { name: 'Hair', value: 0, min: 0, max: 78, increment: 1 }, // 10
-    { name: 'Hair Color 1', value: 0, min: 0, max: 78, increment: 1 }, // 11
-    { name: 'Hair Color 2', value: 0, min: 0, max: 78, increment: 1 }, // 12
-    { name: 'Hair Texture', value: 0, min: 0, max: 0, increment: 1 } // 13
-];
+// List of all the facial features.
+const facialFeatures = {
+    Sex: { value: 0, min: 0, max: 1, increment: 1, func: updateSex }, // 0
+    'Father Face': {
+        value: 0,
+        min: 0,
+        max: 45,
+        increment: 1,
+        func: updatePlayerFace
+    }, // 1
+    'Father Skin': {
+        value: 0,
+        min: 0,
+        max: 45,
+        increment: 1,
+        func: updatePlayerFace
+    }, // 2
+    'Mother Face': {
+        value: 0,
+        min: 0,
+        max: 45,
+        increment: 1,
+        func: updatePlayerFace
+    }, // 3
+    'Mother Skin': {
+        value: 0,
+        min: 0,
+        max: 45,
+        increment: 1,
+        func: updatePlayerFace
+    }, // 4
+    'Extra Face': {
+        value: 0,
+        min: 0,
+        max: 45,
+        increment: 1,
+        func: updatePlayerFace
+    }, // 5
+    'Extra Skin': {
+        value: 0,
+        min: 0,
+        max: 45,
+        increment: 1,
+        func: updatePlayerFace
+    }, // 6
+    'Face Mix': {
+        value: 0,
+        min: 0,
+        max: 1,
+        increment: 0.1,
+        func: updatePlayerFace
+    }, // 7
+    'Skin Mix': {
+        value: 0,
+        min: 0,
+        max: 1,
+        increment: 0.1,
+        func: updatePlayerFace
+    }, // 8
+    'Third Mix': {
+        value: 0,
+        min: 0,
+        max: 1,
+        increment: 0.1,
+        func: updatePlayerFace
+    }, // 9
+    Hair: { value: 0, min: 0, max: 78, increment: 1, func: updateHair }, // 10
+    'Hair Color 1': {
+        value: 0,
+        min: 0,
+        max: 78,
+        increment: 1,
+        func: updateHair
+    }, // 11
+    'Hair Color 2': {
+        value: 0,
+        min: 0,
+        max: 78,
+        increment: 1,
+        func: updateHair
+    }, // 12
+    'Hair Texture': {
+        value: 0,
+        min: 0,
+        max: 0,
+        increment: 1,
+        func: updateHair
+    }, // 13
+    'Eye Color': { value: 0, min: 0, max: 32, increment: 1 }, // 14
+    'Nose Width': { value: 0, min: -1, max: 1, increment: 0.1 }, // 15
+    'Nose Height': { value: 0, min: -1, max: 1, increment: 0.1 }, // 16
+    'Nose Length': { value: 0, min: -1, max: 1, increment: 0.1 }, // 17
+    'Nose Bridge': { value: 0, min: -1, max: 1, increment: 0.1 }, // 18
+    'Nose Tip': { value: 0, min: -1, max: 1, increment: 0.1 }, // 19
+    'Nose Bridge Shaft': { value: 0, min: -1, max: 1, increment: 0.1 }, //20
+    'Brow Height': { value: 0, min: -1, max: 1, increment: 0.1 }, // 21
+    'Brow Width': { value: 0, min: -1, max: 1, increment: 0.1 }, // 22
+    'Cheekbone Height': { value: 0, min: -1, max: 1, increment: 0.1 }, // 23
+    'Cheekbone Width': { value: 0, min: -1, max: 1, increment: 0.1 }, // 24
+    'Cheek Width': { value: 0, min: -1, max: 1, increment: 0.1 }, // 25
+    Eyelids: { value: 0, min: -1, max: 1, increment: 0.1 }, // 26
+    Lips: { value: 0, min: -1, max: 1, increment: 0.1 }, // 27
+    'Jaw Width': { value: 0, min: -1, max: 1, increment: 0.1 }, // 28
+    'Jaw Height': { value: 0, min: -1, max: 1, increment: 0.1 }, // 29
+    'Chin Length': { value: 0, min: -1, max: 1, increment: 0.1 }, // 30
+    'Chin Position': { value: 0, min: -1, max: 1, increment: 0.1 }, // 31
+    'Chin Width': { value: 0, min: -1, max: 1, increment: 0.1 }, // 32
+    'Chin Shape': { value: 0, min: -1, max: 1, increment: 0.1 }, // 33
+    'Neck Width': { value: 0, min: -1, max: 1, increment: 0.1 }, // 34
+    Blemish: { value: 0, min: 0, max: 23, increment: 1 }, // 35
+    'Blemish Opacity': { value: 0, min: 0, max: 1, increment: 0.1 },
+    'Facial Hair': { value: 0, min: 0, max: 28, increment: 1 }, // 37, 2
+    'Facial Hair Opacity': { value: 0, min: 0, max: 1, increment: 0.1 },
+    'Facial Hair Color': { value: 0, min: 0, max: 1, increment: 1 },
+    'Facial Hair Color 2': { value: 0, min: 0, max: 1, increment: 1 },
+    Eyebrows: { value: 0, min: 0, max: 33, increment: 1 }, // 41
+    'Eyebrows Opacity': { value: 0, min: 0, max: 1, increment: 0.1 },
+    'Eyebrows Color': { value: 0, min: 0, max: 1, increment: 1 },
+    'Eyebrows Color 2': { value: 0, min: 0, max: 1, increment: 1 },
+    Age: { value: 0, min: 0, max: 14, increment: 1 }, // 45
+    'Age Opacity': { value: 0, min: 0, max: 1, increment: 0.1 },
+    Makeup: { value: 0, min: 0, max: 74, increment: 1 }, // 47
+    'Makeup Opacity': { value: 0, min: 0, max: 1, increment: 0.1 },
+    'Makeup Color': { value: 0, min: 0, max: 1, increment: 1 },
+    'Makeup Color 2': { value: 0, min: 0, max: 1, increment: 1 },
+    Blush: { value: 0, min: 0, max: 6, increment: 1 }, // 51
+    'Blush Opacity': { value: 0, min: 0, max: 1, increment: 0.1 },
+    'Blush Color': { value: 0, min: 0, max: 1, increment: 1 },
+    Complexion: { value: 0, min: 0, max: 11, increment: 1 }, // 54
+    'Complexion Opacity': { value: 0, min: 0, max: 1, increment: 0.1 },
+    'Sun Damage': { value: 0, min: 0, max: 10, increment: 1 }, // 56
+    'Sun Damage Opacity': { value: 0, min: 0, max: 1, increment: 0.1 },
+    Lipstick: { value: 0, min: 0, max: 9, increment: 1 }, // 58, 22
+    'Lipstick Opacity': { value: 0, min: 0, max: 1, increment: 0.1 },
+    'Lipstick Color': { value: 0, min: 0, max: 1, increment: 1 },
+    'Lipstick Color 2': { value: 0, min: 0, max: 1, increment: 1 },
+    Freckles: { value: 0, min: 0, max: 17, increment: 1 }, // 62, 26
+    'Freckles Opacity': { value: 0, min: 0, max: 1, increment: 0.1 }
+};
 
+// Setup buttons programatically for usage.
 $(() => {
-    for (var index in facialFeatures) {
-        let increment = facialFeatures[index].increment;
-        let name = facialFeatures[index].name;
-        let value = facialFeatures[index].value;
-        let max = facialFeatures[index].max;
-
+    for (let key in facialFeatures) {
         $('#populateButtons').append(`
 			<div class="btn-group w-100 p-2 pl-3 pr-3" role="group">
-				<button type="button" class="btn btn-sm btn-secondary" onclick="changeValue(${index}, false);">&lt;</button>
-				<button type="button" id="button-${index}" class="btn btn-sm btn-block btn-secondary" disabled>${name} <span class="badge badge-secondary">[${value}/${max}]</span></button>
-				<button type="button" class="btn btn-sm btn-secondary" onclick="changeValue(${index}, true);">&gt;</button>
+				<button type="button" class="btn btn-sm btn-secondary" onclick="changeValue(${
+                    facialFeatures[key]
+                }, false);">&lt;</button>
+				<button type="button" id="button-${key}" class="btn btn-sm btn-block btn-secondary" disabled>${key} <span class="badge badge-secondary">[${
+            facialFeatures[key].value
+        }/${facialFeatures[key].max}]</span></button>
+				<button type="button" class="btn btn-sm btn-secondary" onclick="changeValue(${
+                    facialFeatures[key]
+                }, true);">&gt;</button>
 			</div>
   		`);
     }
 });
 
 // Updates the local facial values registered in this WebView
-function changeValue(index, increment) {
+function changeValue(key, increment) {
     if (increment) {
-        facialFeatures[index].value += facialFeatures[index].increment;
+        facialFeatures[key].value += facialFeatures[key].increment;
     } else {
-        facialFeatures[index].value -= facialFeatures[index].increment;
+        facialFeatures[key].value -= facialFeatures[key].increment;
     }
 
-    if (facialFeatures[index].value > facialFeatures[index].max) {
-        facialFeatures[index].value = facialFeatures[index].min;
+    if (facialFeatures[key].value > facialFeatures[key].max) {
+        facialFeatures[key].value = facialFeatures[key].min;
     }
 
-    if (facialFeatures[index].value < facialFeatures[index].min) {
-        facialFeatures[index].value = facialFeatures[index].max;
+    if (facialFeatures[key].value < facialFeatures[key].min) {
+        facialFeatures[key].value = facialFeatures[key].max;
     }
 
-    if (facialFeatures[index].increment === 0.1) {
-        facialFeatures[index].value =
-            Number.parseFloat(facialFeatures[index].value).toFixed(2) * 1;
+    if (facialFeatures[key].increment === 0.1) {
+        facialFeatures[key].value =
+            Number.parseFloat(facialFeatures[key].value).toFixed(2) * 1;
     }
 
-    $(`#button-${index}`).html(
-        `${facialFeatures[index].name} <span class="badge badge-secondary">[${
-            facialFeatures[index].value
-        }/${facialFeatures[index].max}]</span>`
+    // Update the Value of the Key Pressed
+    $(`#button-${key}`).html(
+        `${facialFeatures[key].name} <span class="badge badge-secondary">[${
+            facialFeatures[key].value
+        }/${facialFeatures[key].max}]</span>`
     );
 
-    // Sex
-    if (index === 0) {
-        alt.emit('updateSex', facialFeatures[index].value);
-        setTimeout(() => {
-            updatePlayerFace();
-        }, 1000);
-
-        return;
-    }
-
-    // Update Face (Head Blend Data)
-    if (index >= 1 && index <= 9) {
-        updatePlayerFace();
-        return;
-    }
-
-    if (index >= 10 && index <= 13) {
-        alt.emit(
-            'updateHairStyle',
-            facialFeatures[10].value,
-            facialFeatures[11].value,
-            facialFeatures[12].value,
-            facialFeatures[13].value
-        );
-    }
+    facialFeatures[key].func();
 }
 
-function updatePlayerFace() {
+function updateSex() {
+    alt.emit('updateSex', facialFeatures[key].value);
+    setTimeout(() => {
+        updatePlayerFace();
+        updateHair();
+        updateFacialFeatures();
+        updateFacialDecor();
+    }, 500);
+}
+
+// Update the player's face completely.
+function updatePlayerFace(emitCall, min, max) {
+    var data = [];
+
+    Object.keys(facialFeatures).forEach((key, index) => {
+        if (index > max && index < min) return;
+
+        data.push(facialFeatures[key].value);
+    });
+
+    alt.emit(`${emitCall}`, JSON.stringify(data));
+
     alt.emit(
         'updateHeadBlend',
         facialFeatures[1].value,
@@ -101,12 +224,58 @@ function updatePlayerFace() {
     );
 }
 
+// Update the player's facial details; nose, eyes, etc.
+function updateFacialFeatures() {
+    let arrayOfFaceFeatures = [];
+
+    // Thanks superiouzz; missed the last facial feature.
+    for (var i = 15; i < 35; i++) {
+        arrayOfFaceFeatures.push(facialFeatures[i].value);
+    }
+
+    alt.emit(
+        'updateFacialFeatures',
+        facialFeatures[14].value,
+        JSON.stringify(arrayOfFaceFeatures)
+    );
+}
+
+// Update Hair Style, Color, and Texture
+function updateHair() {
+    alt.emit(
+        'updateHairStyle',
+        facialFeatures[10].value,
+        facialFeatures[11].value,
+        facialFeatures[12].value,
+        facialFeatures[13].value
+    );
+}
+
+function updateFacialDecor() {
+    let arrayOfFacialDecor = [];
+
+    for (var i = 35; i < facialFeatures.length; i++) {
+        arrayOfFacialDecor.push(facialFeatures[i].value);
+    }
+
+    alt.emit('updateFacialDecor', JSON.stringify(arrayOfFacialDecor));
+}
+
 if ('alt' in window) {
     // Grab the Style Variations
     alt.on('stylesUpdate', (beardStyles, hairStyles, hairColors) => {
         facialFeatures[10].max = hairStyles;
-        facialFeatures[11].max = hairColors;
-        facialFeatures[12].max = hairColors;
+
+        for (var i = 0; i < facialFeatures.length; i++) {
+            if (facialFeatures[i].name.toLowerCase().includes('color')) {
+                console.log(facialFeatures[i].name);
+                facialFeatures[i].max = hairColors;
+                $(`#button-${i}`).html(`
+				${facialFeatures[i].name} <span class="badge badge-secondary">[${
+                    facialFeatures[i].value
+                }/${facialFeatures[i].max}]</span>`);
+            }
+        }
 
         for (var i = 10; i < 12; i++) {
             $(`#button-${i}`).html(`
@@ -116,6 +285,7 @@ if ('alt' in window) {
         }
     });
 
+    // Called when the player changes their hair so we can setup new hair texture variations.
     alt.on('setHairTextureVariations', hairTextureVariations => {
         facialFeatures[13].max = hairTextureVariations;
         facialFeatures[13].value = 0;

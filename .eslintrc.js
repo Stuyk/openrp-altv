230ad6c0b29843382210fcd7f3f060dc37338ed3
Stuyk <stuykgaming@gmail.com>
2019-08-26 17:14:20 -0600

    ESLINT

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        commonjs: true
+    },
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: false
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module'
+    },
+    plugins: [],
+    rules: {}
+};

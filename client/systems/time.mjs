b52e779bea4a17dfc47ef4d1ec2ad80cef313258
Stuyk <stuykgaming@gmail.com>
2019-09-01 12:48:32 -0600

    Time Change to 30 Minutes for 1 Hour

---
+++ client/systems/time.mjs
@@ -0,0 +1,13 @@
+import * as alt from 'alt';
+
+let currentHour = 8;
+
+alt.on('syncedMetaChange', (entity, key, value) => {
+    if (key !== 'time') return;
+
+    currentHour = value;
+});
+
+alt.setInterval(() => {
+    native.networkOverrideClockTime(8, 0, 0);
+}, 1000);
--- server/systems/time.mjs
+++ server/systems/time.mjs
@@ -2,22 +2,29 @@ import * as alt from 'alt';
 
 console.log('Loaded: systems->time.mjs');
 
-let lastDate = new Date();
+let time = {
+    hour: 8
+};
+
+let lastUpdate = Date.now() + 1800000; // 30 Minutes = 1 Hour
 
 export function setTimeForNewPlayer(player) {
-    player.setDateTime(
-        lastDate.getDay(),
-        lastDate.getMonth(),
-        lastDate.getFullYear(),
-        lastDate.getHours(),
-        lastDate.getMinutes(),
-        lastDate.getSeconds()
-    );
+    let day = new Date();
+    player.setDateTime(day.getDay(), day.getMonth(), day.getFullYear(), time.hour, 0, 0);
+    player.setSyncedMeta('time', time.hour);
 }
 
 setInterval(() => {
+    if (Date.now() < lastUpdate) return;
+
+    lastUpdate = Date.now() + 1800000;
+    time.hour += 1;
+
+    if (time.hour >= 24) {
+        time.hour = 0;
+    }
+
     alt.Player.all.forEach(player => {
-        lastDate = new Date();
         setTimeForNewPlayer(player);
     });
 }, 60000);

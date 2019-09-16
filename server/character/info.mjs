4d9828d0e430460cecadca4ee2900b401cdc53cf
dexslab <dex35803@gmail.com>
2019-09-16 17:53:30 -0400

    Switch to truely random idnum generation

--- server/character/info.mjs
+++ server/character/info.mjs
@@ -1,21 +1,24 @@
 import * as alt from 'alt';
 import SQL from '../../../postgres-wrapper/database.mjs';
 import * as cache from '../cache/cache.mjs';
+import { generateHash } from '../utility/encryption.mjs';
 
 const db = new SQL();
 
 console.log('Loaded: character->info.mjs');
 
 export function setRoleplayInfo(player, info) {
-
     player.needsRoleplayInfo = false;
 
-    let randid = Math.floor(100000000 + Math.random() * 900000000);
+    let randid = generateHash(info.name);
+    var matches = str.match(/(\d+9)/);
+
+    if (matches) {
+        randid = matches[0];
+    }
     // Cache the idnum.
     cache.cacheIdNum(randid);
     // Sets and saves the player's roleplay name.
-    player.saveRoleplayInfo({name: info.name, dob: info.dob, idnum: randid.toString()});
+    player.saveRoleplayInfo({ name: info.name, dob: info.dob, idnum: randid.toString() });
     player.closeRoleplayInfoDialogue();
 }
-
-

d861dd848d9916b9c90912461991e15a6a011004
Stuyk <stuykgaming@gmail.com>
2019-10-06 15:08:26 -0600

    Fix Anti-Cheat Wep Issue

--- server/events/weaponDamage.mjs
+++ server/events/weaponDamage.mjs
@@ -2,7 +2,7 @@ import * as alt from 'alt';
 import { verifyWeapon } from '../systems/anticheat.mjs';
 
 alt.on('weaponDamage', (attacker, target, weapon, damage, offset, bodyPart) => {
-    if (weapon !== 2725352035) {
+    if (weapon !== 133987706 && weapon !== 2741846334 && weapon !== 2725352035) {
         if (!verifyWeapon(attacker)) {
             return false;
         }

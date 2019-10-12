7ed9bac4b54e4dccf7e4d3abfb5b8036beffa883
Jason Hines <jason@greenhell.com>
2019-10-12 14:22:55 -0400

    adding tpwp command to sandbox

--- client/utility/sandbox.mjs
+++ client/utility/sandbox.mjs
@@ -6,3 +6,16 @@ alt.onServer('getForwardVector', () => {
     var forward = native.getEntityForwardVector(alt.Player.local.scriptID);
     alt.emitServer('getForwardVector', forward);
 });
+
+alt.onServer('teleportToWaypoint', player => {
+    if (!native.isWaypointActive()) return;
+
+    let wp = native.getFirstBlipInfoId(8);
+    let coords = native.getBlipInfoIdCoord(wp);
+    // Will fall through map until eventually lands on ground.
+    native.doScreenFadeOut(250);
+    native.setEntityCoords(alt.Player.local.scriptID, coords.x, coords.y, coords.z, 1, 0, 0, 1);
+    alt.setTimeout(function () {
+        native.doScreenFadeIn(250);
+    }, 2000);
+});

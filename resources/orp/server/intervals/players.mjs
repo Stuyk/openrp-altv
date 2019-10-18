ebc4a8fd3ef55076c7b20f856244804bfbb60244
Stuyk <stuykgaming@gmail.com>
2019-10-18 11:59:25 -0600

    Make Intervals Consistent

--- resources/orp/server/intervals/players.mjs
+++ resources/orp/server/intervals/players.mjs
@@ -7,22 +7,24 @@ setInterval(() => {
     for (let i = 0; i < alt.Player.all.length; i++) {
         if (!alt.Player.all[i].data) continue;
         const player = alt.Player.all[i];
+        const now = Date.now();
 
-        player.saveData();
-        if (nextSavePlayerPlayTime < Date.now()) {
-            nextSavePlayerPlayTime = Date.now() + 60000 * 5;
-            player.updatePlayingTime();
+        if (player.saveData) player.saveData();
+
+        if (nextSavePlayerPlayTime < now) {
+            nextSavePlayerPlayTime = now + 60000 * 5;
+            if (player.updatePlayingTime) player.updatePlayingTime();
         }
 
         if (
-            Date.now() > parseInt(player.data.arrestTime) &&
+            now > parseInt(player.data.arrestTime) &&
             parseInt(player.data.arrestTime) !== -1
         ) {
-            player.setArrestTime(-1);
+            if (player.setArrestTime) player.setArrestTime(-1);
         }
 
         if (player.reviveTime) {
-            if (Date.now() > player.reviveTime) {
+            if (now > player.reviveTime && player.revive) {
                 player.revive();
             }
         }

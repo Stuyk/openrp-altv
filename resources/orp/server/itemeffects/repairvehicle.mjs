5a93b5336c743e683b0b8994f3641630b8a232ff
Jason Hines <jason@greenhell.com>
2019-10-19 13:51:43 -0400

    use repairkit

--- resources/orp/server/itemeffects/repairvehicle.mjs
+++ resources/orp/server/itemeffects/repairvehicle.mjs
@@ -1,11 +1,14 @@
 import * as alt from 'alt';
 
-alt.on('itemeffects:RepairVehicle', (player, itemData) => {
+alt.on('itemeffects:RepairVehicle', (player, item, hash) => {
+    if (!player.subItem(item.key, 1)) {
+        return;
+    }
+
     player.send('You begin repairing the vehicle.');
 
     // TODO: couldn't get animation to work :(
     //player.playAnimation = ('mini@repair', 'fixing_a_car', 1200, 33);
 
-    //player.send(`${JSON.stringify(itemData)}`);
     alt.emitClient(player, 'inventory:UseRepairKit');
 });

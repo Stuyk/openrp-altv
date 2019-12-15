c4b60c3ffeb78eb32952e00a07d6d9960e08874f
Stuyk <stuykgaming@gmail.com>
2019-12-15 11:48:47 -0700

    Add Lenght Restrictions

--- resources/orp/client/html/roleplayinfo/app.js
+++ resources/orp/client/html/roleplayinfo/app.js
@@ -27,7 +27,7 @@ class App extends Component {
         const firstName = e.target.value;
         const nameCapitalized = firstName.charAt(0).toUpperCase() + firstName.slice(1);
 
-        if (!regex.test(nameCapitalized)) {
+        if (!regex.test(nameCapitalized) || nameCapitalized.length > 12) {
             this.setState({ firstName: this.state.firstName });
             return;
         }
@@ -39,22 +39,12 @@ class App extends Component {
         const lastName = e.target.value;
         const nameCapitalized = lastName.charAt(0).toUpperCase() + lastName.slice(1);
 
-        if (!regex.test(nameCapitalized)) {
+        if (!regex.test(nameCapitalized) || nameCapitalized.length > 12) {
             this.setState({ lastName: this.state.lastName });
             return;
         }
 
         this.setState({ lastName: nameCapitalized });
-    }
-
-    filterFirstNames(e) {
-        const value = e.target.value;
-        this.setState({ firstFilter: value });
-    }
-
-    filterLastNames(e) {
-        const value = e.target.value;
-        this.setState({ lastFilter: value });
     }
 
     submit() {
@@ -69,7 +59,9 @@ class App extends Component {
     render() {
         const firstName = this.state.firstName === '' ? 'Select' : this.state.firstName;
         const lastName = this.state.lastName === '' ? 'Name' : this.state.lastName;
-        const allValid = firstName !== 'Select' && lastName !== 'Name' ? true : false;
+        let allValid = firstName !== 'Select' && lastName !== 'Name' ? true : false;
+        allValid = firstName.length >= 2 && lastName.length >= 2 ? true : false;
+
         return h(
             'div',
             { id: 'app' },
